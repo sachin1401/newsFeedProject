9038d03e15a3f10c076770f9f6b3af9476049746
sachin1401 <sachinpatel7410@gmail.com>
2023-07-15 13:39:00 +0530

    update js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -21,23 +21,28 @@ fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=
     });
 
     let newsContainer = document.getElementById("newsContainer");
-    newsContainer.innerHTML = tableData.join("");
+    newsContainer.innerHTML = tableData.join(" ");
 
     // Hide the loading icon once the data is shown
     loadingIcon.style.display = "none";
   })
   .catch((error) => {
     // Handle any errors that occur during the request
-    console.error('Error:', error);
+    // console.error('Error:', error);
 
     // Hide the loading icon in case of an error
     loadingIcon.style.display = "none";
   });
+
+
+
+
 // ************************************
 // category section
 
 const categorySelect = document.getElementById("categorySelect");
 const newsContainer = document.getElementById("newNewsContainer");
+const categoryPrint = document.getElementById("categoryPrint");
 // const loadingIcon = document.getElementById("loadingIcon");
 // loadingIcon.style.display = "block";
 
@@ -52,6 +57,7 @@ categorySelect.addEventListener("change", () => {
     .then((response) => response.json())
     .then((data) => {
       let tableData = data.articles.map((article) => {
+        categoryPrint.textContent = selectedCategory;
         return `
           <div class="news-card">
             <div class="news-image-container">
@@ -65,87 +71,17 @@ categorySelect.addEventListener("change", () => {
         `;
       });
 
-      newsContainer.innerHTML = tableData.join("");
+      newsContainer.innerHTML = tableData.join(" ");
 
       // Hide the loading icon once the data is shown
       loadingIcon.style.display = "none";
     })
     .catch((error) => {
       // Handle any errors that occur during the request
-      console.error('Error:', error);
+      // console.error('Error:', error);
 
       // Hide the loading icon in case of an error
       loadingIcon.style.display = "none";
     });
 });
-
-
-
-
-// const loadingIcon = document.getElementById("loadingIcon");
-
-// // Show the loading icon
-// loadingIcon.style.display = "block";
-
-// fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b958ba6bcead4c1b8f45d7f5d64b4e96")
-//   .then((data) => { 
-//     return data.json();
-//   }) 
-//   .then((objectData) => {
-//     let tableData = objectData.data.map((values) => {
-//       return `        <div class="news-card">
-//                       <div class="news-image-container">
-//                         <img class="news-image" src="${values.imageUrl}" alt="News Image"/>
-//                       </div>
-//                       <div class="news-content">
-//                         <h3 class="news-author alert alert-primary"> ${values.title}</h3>
-//                         <p class="news-text">${values.content}</p>
-//                       </div>
-//                     </div>
-//               `;
-//     });
-//     let value = document.getElementById("newsContainer");
-//     value.innerHTML = tableData;
-
-//     // Hide the loading icon once the data is shown
-//     loadingIcon.style.display = "none";
-//   });
-
-
-// new news load category
-// const categorySelect = document.getElementById("categorySelect");
-// const newsContainer = document.getElementById("newNewsContainer");
-
-// categorySelect.addEventListener("change", () => {
-//   const selectedCategory = categorySelect.value;
-//   const apiUrl = `https://inshorts.deta.dev/news?category=${selectedCategory}`;
-
-//   fetch(apiUrl) 
-//     .then(response => response.json())
-//     .then(objectData => {
-//       const newsCards = objectData.data.map(values => {
-//         return `        
-//           <div class="news-card">
-//             <div class="news-image-container">
-//               <img class="news-image" src="${values.imageUrl}" alt="News Image"/>
-//             </div>
-            
-//             <div class="news-content">
-                            
-//               <h3 class="news-author alert alert-primary">${values.title}</h3>
-//               <p class="news-text">${values.content}</p>
-//               <a class="read-more-link" href="#" >${values.time}</a>            
-//             </div>
-//           </div>
-//         `;
-//       });
-
-//       newsContainer.innerHTML = newsCards.join("");
-//     })
-//     .catch(error => {
-//       console.log("Error fetching API data:", error);
-//     });
-//     loadingIcon.style.display = "none";
-// });
-
 
